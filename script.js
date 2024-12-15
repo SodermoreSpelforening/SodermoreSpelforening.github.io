e198e0e0c8c886ee102ae357af076b942ec4fef8
Södermöre Spelförening <sodermorespelforening@gmail.com>
2024-12-15 11:25:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript är inte strikt nödvändigt för denna sida, men vi kan lägga till några enkla funktioner här
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Webbsidan har laddats!");
+});
